76cf93b825d285b4de96242bdc488312cc7e8895
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-17 20:36:51 +0100

    Code reformatted with Blue Style

--- day17.jl
+++ day17.jl
@@ -12,7 +12,11 @@ const sum_states3D = tuple(false, false, false, true, zeros(Bool, 23)...),
 tuple(false, false, true, true, zeros(Bool, 23)...)
 
 initstate3D = reshape(fill(false, maxsz^3), maxsz, maxsz, maxsz)
-view(initstate3D, maxsz÷2-4:maxsz÷2+3, maxsz÷2-4:maxsz÷2+3, maxsz ÷ 2) .= initstate2D
+function view(
+    initstate3D, (maxsz ÷ 2 - 4):(maxsz ÷ 2 + 3), (maxsz ÷ 2 - 4):(maxsz ÷ 2 + 3), maxsz ÷ 2
+)
+    return initstate2D
+end
 # Moves the initial state 8 x 8, as a 1-dim slice,
 # to the center of the 3D initial state
 
@@ -20,32 +24,31 @@ function sim1(state, cycles)
     @inline function offsets()
         # Returns the Moore neighborhood in 3D as an iterable of offsets
 
-        ((i, j, k) for k in -1:1, j in -1:1, i in -1:1 if (i, j, k) != (0, 0, 0))
+        return ((i, j, k) for k in -1:1, j in -1:1, i in -1:1 if (i, j, k) != (0, 0, 0))
     end
 
     function hood(grid, i)
         # Return the state of the Moore neighbors of cell i in grid
 
         c = Tuple(i)
-        [grid[CartesianIndex(c .+ i)] for i in offsets()]
+        return [grid[CartesianIndex(c .+ i)] for i in offsets()]
     end
 
-    life(hood, cstate) = sum_states3D[cstate+1][sum(hood)+1]
+    life(hood, cstate) = sum_states3D[cstate + 1][sum(hood) + 1]
 
-    for cycle = 1:cycles
+    for cycle in 1:cycles
         next = reshape(fill(false, maxsz^3), maxsz, maxsz, maxsz)
-        for i in CartesianIndices((2:maxsz-1, 2:maxsz-1, 2:maxsz-1))
+        for i in CartesianIndices((2:(maxsz - 1), 2:(maxsz - 1), 2:(maxsz - 1)))
             # For every cell in the state calculates the life of next generation
 
             next[i] = life(hood(state, i), state[i])
         end
         next, state = state, next
     end
-    state
+    return state
 end
 
 sum(sim1(initstate3D, 6))
-
 
 # Second Half
 
@@ -53,13 +56,15 @@ const sum_states4D = tuple(false, false, false, true, zeros(Bool, 77)...),
 tuple(false, false, true, true, zeros(Bool, 77)...)
 
 initstate4D = reshape(fill(false, maxsz^4), maxsz, maxsz, maxsz, maxsz)
-view(
+function view(
     initstate4D,
-    maxsz÷2-4:maxsz÷2+3,
-    maxsz÷2-4:maxsz÷2+3,
-    maxsz÷2-4:maxsz÷2+3,
+    (maxsz ÷ 2 - 4):(maxsz ÷ 2 + 3),
+    (maxsz ÷ 2 - 4):(maxsz ÷ 2 + 3),
+    (maxsz ÷ 2 - 4):(maxsz ÷ 2 + 3),
     maxsz ÷ 2,
-) .= initstate2D
+)
+    return initstate2D
+end
 # Moves the initial state 8 x 8, as a 1-dim slice,
 # to the center of the 4D initial state
 
@@ -67,7 +72,7 @@ function sim2(state, cycles)
     @inline function offsets()
         # Returns the Moore neighborhood in 4D as an iterable of offsets
 
-        (
+        return (
             (i, j, k, l) for
             l in -1:1, k in -1:1, j in -1:1, i in -1:1 if (i, j, k, l) != (0, 0, 0, 0)
         )
@@ -77,21 +82,22 @@ function sim2(state, cycles)
         # Return the state of the Moore neighbors of cell i in grid
 
         c = Tuple(i)
-        [grid[CartesianIndex(c .+ i)] for i in offsets()]
+        return [grid[CartesianIndex(c .+ i)] for i in offsets()]
     end
 
-    life(hood, cstate) = sum_states4D[cstate+1][sum(hood)+1]
+    life(hood, cstate) = sum_states4D[cstate + 1][sum(hood) + 1]
 
-    for cycle = 1:cycles
+    for cycle in 1:cycles
         next = reshape(fill(false, maxsz^4), maxsz, maxsz, maxsz, maxsz)
-        for i in CartesianIndices((2:maxsz-1, 2:maxsz-1, 2:maxsz-1, 2:maxsz-1))
+        for i in
+            CartesianIndices((2:(maxsz - 1), 2:(maxsz - 1), 2:(maxsz - 1), 2:(maxsz - 1)))
             # For every cell in the state calculates the life of next generation
 
             next[i] = life(hood(state, i), state[i])
         end
         next, state = state, next
     end
-    state
+    return state
 end
 
 sum(sim2(initstate4D, 6))
