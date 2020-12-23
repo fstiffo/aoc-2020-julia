34f1296d0680feddd9563b956c076ea682e0e5a5
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-23 17:04:40 +0100

    Second half solution day 23

--- day23.jl
+++ day23.jl
@@ -11,7 +11,7 @@ const maxlbl1 = 9
 
 function simulate1!(cups, moves)
 
-    function place(cups, pickup, destination)
+    function place(cups, pickup, pckpos, destination)
         t = Vector(cups)
         for p in pickup
             pckpos = findfirst(x -> x == p, t)
@@ -73,52 +73,60 @@ const maxlbl2 = 1000000
 
 ~(x::Int, y::Int) = mod1(x - y, maxlbl2) # Circular subtraction
 
-function simulate2!(cups, moves)
+function simulate2!(labeling, moves)
 
-    function place(cups, pickup, destination)
-        t = Vector(cups)
-        for p in pickup
-            pckpos = findfirst(x -> x == p, t)
-            deleteat!(t, pckpos)
-        end
-        destpos = findfirst(x -> x == destination, t)
-        CircularVector([t[1:destpos]; pickup; t[destpos+1:end]])
+    cups = collect([2:maxlbl2+1]...)
+    labeling = [labeling;length(labeling)+1]
+    for i in 1:9
+        n = labeling[i]
+        nx = labeling[i+1]
+        cups[n] = nx
     end
+    cups[maxlbl2] = labeling[1]
+    # The cups arrya store at position n the label of the cup immediately
+    # clockwise of cup labeled n, that make all so simple!
 
-    current = cups[1]
-    destination = 0
+
+    cur = labeling[1]
+    des = 0
+    pck = [0,0,0]
     for m = 1:moves
-        currpos = findfirst(x -> x == current, cups)
-        pickup = cups[currpos+1:currpos+3]
+        pck[1] = cups[cur]
+        pck[2] = cups[pck[1]]
+        pck[3] = cups[pck[2]]
         # Step 1 - The crab picks up the three cups that are immediately
         # clockwise of the current cup. They are removed from the circle;
 
-
-        destination = current ~ 1
+        des = cur ~ 1
         # Step 2 - The crab selects a destination cup: the cup with a label
         # equal to the current cup's label minus one.
 
-        while destination ∈ pickup
+        while des ∈ pck
             # If this would select one of the cups that was just picked up,
             # the crab will keep subtracting one until it finds a cup that
             # wasn't just picked up.
 
-            destination = destination ~ 1
+            des = des ~ 1
         end
 
-        cups = place(cups, pickup, destination)
+        nexttodes = cups[des]
+        cups[des] = pck[1]
+        cups[cur] = cups[pck[3]]
+        cups[pck[3]] = nexttodes
         # Step 3 - The crab places the cups it just picked up so that they are
         # immediately clockwise of the destination cup. They keep the same
         # order as when they were picked up.
 
-        currpos = findfirst(x -> x == current, cups)
-        current = cups[currpos+1]
+        cur = cups[cur]
         # Step 4 - The crab selects a new current cup: the cup which is
         # immediately clockwise of the current cup.
     end
-    cups
+
+    cups[1] * cups[cups[1]]
 end
 
-puzzleinput2 = CircularVector([[6, 8, 5, 9, 7, 4, 2, 1, 3]; collect([10:maxlbl2]...)])
 
-simulate2!(puzzleinput2, 10000000)
+puzzletestinput = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+puzzleinput = [6, 8, 5, 9, 7, 4, 2, 1, 3]
+
+@time simulate2!(puzzleinput, 10000000)
