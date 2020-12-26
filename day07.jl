042b16594f0d54299d457342c7fa0a66da0d3a15
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-26 10:57:50 +0100

    First Half solution day 07

--- day07.jl
+++ day07.jl
@@ -1,58 +1,86 @@
-using DataStructures
+using LightGraphs
+using GraphPlot
 
-puzzleinput = readlines("inputs/day07.txt")
 
-function parse_contents(s)::Dict{String,Int}
-    contents = Dict{String,Int}()
-    bs = split(s, ",")
-    if bs[1] == "no other bags"
-        return contents
-    else
-        for b in bs
-            m = match(r".*(\d+)\s(.+) bag.*", b)
-            contents[m[2]] = parse(Int, m[1])
-        end
-    end
-    contents
-end
+function readinput(strs)
 
-function parse_rules!(rules::Dict{String,Accumulator{String,Int64}}, s::String)
-    m = match(r"(.+) bags contain (.+).", s)
-    rules[m[1]] = Accumulator(parse_contents(m[2]))
-end
-
-rules = Dict{String,Accumulator{String,Int64}}()
-(s -> parse_rules!(rules, s)).(puzzleinput)
-
-function test()
-    can_contain = Set()
-    for (color, contents) in rules
-        if isempty(contents) || color == "shiny gold"
-            continue
-        end
-        no_more = false
-        considered = Set()
-        println(color)
-        cont = deepcopy(contents)
-        while !no_more
-            if cont["shiny gold"] > 0
-                no_more = true
-                push!(can_contain, color)
-            else
-                for (c, n) in cont
-                    no_more = true
-                    if !(c ∈ considered)
-                        no_more = false
-                        push!(considered, c)
-                        merge!(cont, rules[c])
-                    end
-                end
+    function parse_contents(s)::Dict{String,Int}
+        contents = Dict{String,Int}()
+        bs = split(s, ",")
+        if bs[1] == "no other bags"
+            return contents
+        else
+            for b in bs
+                m = match(r".*(\d+)\s(.+) bag.*", b)
+                contents[m[2]] = parse(Int, m[1])
             end
         end
+        contents
     end
-    can_contain
+
+    function parse_rules!(rules, vertex, s)
+        m = match(r"(.+) bags contain (.+).", s)
+        rules[m[1]] = (vertex = vertex, contents = parse_contents(m[2]))
+        m[1]
+    end
+
+    rules = Dict{String,@NamedTuple{vertex::Int, contents::Dict{String,Int64}}}()
+    vtxclr = Vector{String}(undef, length(strs))
+
+    for (i, s) in enumerate(strs)
+        vtxclr[i] = parse_rules!(rules, i, s)
+    end
+
+    rulesgrf = SimpleDiGraph(length(strs))
+
+    for (_, r) in rules
+        rv = r.vertex
+        cs = r.contents
+        for (color, _) in pairs(r.contents)
+            add_edge!(rulesgrf, rv, rules[color].vertex)
+        end
+    end
+
+    return rules, vtxclr, rulesgrf
 end
 
-test()
 
-count(r -> r[2]["shiny gold"] > 0, rules)riu
+# First Half
+
+function howmany_contains(rules, vtxclr, rulesgrf, color)
+
+    function add_predecessors!(v, preds)
+        inns = inneighbors(rulesgrf, v)
+        if isempty(inns)
+            return
+        end
+        push!(preds, inns...)
+        for nv in inns
+            add_predecessors!(nv, preds)
+        end
+        return
+    end
+
+    v = rules[color].vertex
+    containers = Set()
+    add_predecessors!(v, containers)
+    length(containers)
+end
+
+puzzleinput = readlines("inputs/day07.txt")
+(rules, vtxclr, rulesgrf) = readinput(puzzleinput)
+
+howmany_contains(rules, vtxclr, rulesgrf, "shiny gold")
+
+
+# Second Half
+
+function howmany_inside(args)
+    body
+end
+
+
+puzzleinput = readlines("inputs/day07.txt")
+(rules, vtxclr, rulesgrf) = readinput(puzzleinput)
+
+howmany_inside(rules, vtxclr, rulesgrf, "shiny gold")
