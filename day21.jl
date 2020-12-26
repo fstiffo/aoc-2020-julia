8b2198cb6dce87adfa80e14db023fa5b6b69ff40
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-26 19:24:26 +0100

    First half solution day 20

--- day21.jl
+++ day21.jl
@@ -1,55 +1,53 @@
 Ingredient = String
 Allergen = String
 
-Allergens = Set{Allergen}
+Ingredients = Set{Ingredient}
+Allergens = Set{Ingredient}
+Foods = Vector{@NamedTuple{ingrs::Ingredients, allrgs::Allergens}}
 
-function readinput!(foodlist, textinput)
-    for l in textinput
-        m = match(r"(.+) \(contains (.+)\)", l)
-        ingredients = split(m[1], " ")
-        allergens = Set(split(m[2], ", "))
-        contains = Dict([(i, allergens) for i in ingredients])
-        push!(foodlist, contains)
+function readinput(strs)
+
+    ingredients = Ingredients()
+    allergens = Allergens()
+    foods = []
+
+    for s in strs
+        m = match(r"(.+) \(contains (.+)\)", s)
+        ingrs = Ingredients(split(m[1], " "))
+        allrgs = Allergens(split(m[2], ", "))
+        union!(ingredients, ingrs)
+        union!(allergens, allrgs)
+        t = (ingrs = ingrs, allrgs = allrgs)
+        push!(foods, t)
     end
+    return ingredients, allergens, foods
 end
 
-
-
-puzzleinput = readlines("inputs/day21.txt")
-
-foodlist = Vector{Dict{Ingredient,Allergens}}()
-
-readinput!(foodlist, puzzleinput)
-
-foodlist
-
-function freeofallergens!(foodlist)
-    free = []
-    for l = 2:length(foodlist)
-        for (food, allergens) in foodlist[l]
-            prevs = foodlist[1:l-1]
-            for prev in prevs
-                if haskey(prev, food)
-                    intersect!(allergens, prev[food])
-                    if isempty(allergens) # The food can't contain allergens
-                        push!(free, food)
-                        map(foodlist) do l
-                            delete!(l, food)
-                        end
-                    else # Update the possible allergens contained by food
-                        foodlist[l][food] = allergens
-                        map(prevs) do p
-                            p[food] = allergens
-                        end
-                    end
-                end
-                # print("Nay!")
-            end
+function foundsin!(allergens, foods)
+    foundsin = Ingredients()
+    prevlen = -1
+    while prevlen < length(foundsin)
+        prevlen = length(foundsin)
+        possiblyin = Dict()
+        for a in allergens
+            contains_a = [ingrs for (ingrs, allrgs) in foods if a ∈ allrgs]
+            p = intersect(contains_a...)
+            possiblyin[a] = p
+        end
+        only1ingr =
+            Set([collect(ingrs)[1] for (_, ingrs) in possiblyin if length(ingrs) == 1])
+        union!(foundsin, only1ingr)
+        for (ingrs, _) in foods
+            setdiff!(ingrs, foundsin)
         end
     end
-    free
+    foundsin
 end
 
-freeofallergens = freeofallergens!(foodlist)
+puzzleinput = readlines("inputs/day21.txt")
+(ingredients, allergens, foods) = readinput(puzzleinput)
 
-sum(sum([occursin.(f, puzzleinput) for f in freeofallergens]))
+foundsin = foundsin!(allergens, foods)
+cantcontain = setdiff(ingredients, foundsin)
+
+sum(s -> length(s.ingrs ∩ cantcontain), foods )
