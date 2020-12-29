69bb0b7e7d606de7da35738c297812154930170a
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-29 13:29:37 +0100

    Second half solutions day 9

--- day09.jl
+++ day09.jl
@@ -14,17 +14,14 @@ function givessum(list, sum)
     nothing
 end
 
+function firstinvalid(list, pmblsz)
+    # Returns first invalid number, if none returns nothing.
 
-# First Half
-
-function firstinvalid(list, preamble_len)
-    # Return first invalid number, if none returns nothing.
-
-    for (i, n) in enumerate(list[preamble_len+1:end])
-        if isnothing(givessum(list[i:i+preamble_len - 1], n))
-            # XMAS starts by transmitting a preamble of preamble_len numbers.
+    for (i, n) in enumerate(list[pmblsz+1:end])
+        if isnothing(givessum(list[i:i+pmblsz-1], n))
+            # XMAS starts by transmitting a preamble of pmblsz numbers.
             # After that, each number you receive should be the sum of any two of
-            # the preamble_len immediately previous numbers.
+            # the pmblsz immediately previous numbers.
 
             return n
         end
@@ -32,5 +29,38 @@ function firstinvalid(list, preamble_len)
     nothing
 end
 
+
+# First Half
+
 puzzleinput = readdlm("inputs/day09.txt", Int)
 firstinvalid(puzzleinput, 25)
+
+
+# Second Half
+
+function findweakness(list, pmblsz)
+    # Returns the encryption weakness in your XMAS-encrypted list of numbers,
+    # if nonte returns nothing
+
+    invalnum = firstinvalid(list, pmblsz)
+    if !isnothing(invalnum)
+        for i in 1:length(list)-1
+            for j in i+1:length(list)
+                contigset = list[i:j]
+                if sum(contigset) == invalnum
+                    # Find a contiguous set of at least two numbers in your list
+                    # which sum to invalnum (the invalid number found before)
+
+                    return minimum(contigset) + maximum(contigset)
+                    # To find the encryption weakness, add together the smallest
+                    # and largest number in this contiguous set
+
+                end
+            end
+        end
+    end
+    nothing
+end
+
+puzzleinput = readdlm("inputs/day09.txt", Int)
+findweakness(puzzleinput, 25)
