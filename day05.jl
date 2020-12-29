3c2618a938318c2b24b4041079bf64575aa23902
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-29 22:05:36 +0100

    Second half solution day 5

--- day05.jl
+++ day05.jl
@@ -1,31 +1,45 @@
-function binparts(s)
-    rowbinpart = s[1:7]
-    rowbinpart = replace(rowbinpart,"B" => "1")
-    rowbinpart = replace(rowbinpart, "F" => "0")
-    colbinpart = s[8:end]
-    colbinpart = replace(colbinpart,"R"=>"1")
-    colbinpart = replace(colbinpart,"L"=>"0")
-    parse(UInt8, rowbinpart, base = 2), parse(UInt8, colbinpart, base = 2)
+function rowcol(s)
+    # Find row and col of the seatid
+
+    rowpart = s[1:7]
+    rowpart = replace(rowpart, "B" => "1")
+    rowpart = replace(rowpart, "F" => "0")
+    colpart = s[8:end]
+    colpart = replace(colpart, "R" => "1")
+    colpart = replace(colpart, "L" => "0")
+    rowbin, colbin = parse(UInt8, rowpart, base = 2), parse(UInt8, colpart, base = 2)
+    Int.((rowbin & UInt(127), colbin & UInt(7)))
 end
+
+seatid((row, col)) = row * 8 + col
+# Every seat also has a unique seat ID: multiply the row by 8, then add the column
+
 
 
 # First Half
 
-function highestid(strs)
-    binarypartions = map(binparts, strs)
+puzzeinput = readlines("inputs/day05.txt")
+maximum(map(seatid ∘ rowcol, puzzeinput))
 
-    function seatid((rowbinpart, colbinpart))
-        (row, col) = rowbinpart & UInt(127), colbinpart & UInt(7)
-        # Find row and col of the seatid
 
-        Int(row) * 8 + Int(col)
-        # Every seat also has a unique seat ID:  multiply the row by 8,
-        # then add the column
+# Second Half
 
+function missingid(strs)
+
+    rowcols = map(rowcol, strs)
+    filter!(rc -> rc[1] > 0 && rc[1] < 127, rowcols)
+    # Removes very front and very back rows, because your seat wasn't
+    # at the very front or back
+
+    sorted_ids = map(seatid, rowcols) |> sort
+    for (i, id) in enumerate(sorted_ids)
+        if sorted_ids[i+1] ≠ id + 1
+            # Missing ID found
+
+            return id + 1
+        end
     end
-
-    maximum(map(seatid, binarypartions))
 end
 
 puzzeinput = readlines("inputs/day05.txt")
-highestid(puzzeinput)
+missingid(puzzeinput)
