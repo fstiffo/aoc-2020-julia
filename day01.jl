c5539aac0dbea4cf7d1c43de996626aeb2746ccf
Francesco Stiffoni <fstiffo@sgajo.com>
2020-12-30 16:03:33 +0100

    Typos

--- day01.jl
+++ day01.jl
@@ -1,1 +1,12 @@
 using DelimitedFiles
+
+
+# First Half
+
+exprpt = readdlm("inputs/day01.txt", Int)
+[i * j for (i, j) in Iterators.product(exprpt, exprpt) if i + j == 2020][1]
+
+
+# Second Half
+exprpt = readdlm("inputs/day01.txt", Int)
+[i * j * k for (i, j, k) in Iterators.product(exprpt, exprpt, exprpt) if i + j + k == 2020][1]
