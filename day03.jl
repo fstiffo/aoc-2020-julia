03b0afbb43cc058dee324a1a9297d812fe2e14f0
Francesco Stiffoni <fstiffo@sgajo.com>
2021-01-01 19:51:47 +0100

    Update comment

--- day03.jl
+++ day03.jl
@@ -8,16 +8,18 @@ end
 # First Half
 
 function treesencountered1(tmap)
-    # The trees map of the problem can be projected on cilindrical discrete plane
-    # where cilinder's height and perimeter are respectively mapₕ and mapₗ.
-    # The movement happens on a line on a discrete plane.
-    # The equation for a line starting at (0,0) in a cilindrical semi-infinite
-    # discrete plane is x = m * y mod mapₗ, where x, y are integers,
-    # slope m is a rational and m * y is rounded to the nearest integer.
-    # In our case m = mₕ / mᵥ (right slope / down slope) and the path is not
-    # this line, but only the dots on this line with vertical coordinate
-    # multiple of mᵥ (jumping).
-
+    #= 
+    The trees map of the problem can be projected on cilindrical discrete plane
+    where cilinder's height and perimeter are respectively mapₕ and mapₗ.
+    The movement happens on a line on a discrete plane.
+    The equation for a line starting at (0,0) in a cilindrical semi-infinite
+    discrete plane is x = m * y mod mapₗ, where x, y are integers,
+    slope m is a rational and m * y is rounded to the nearest integer.
+    In our case m = mₕ / mᵥ (right slope / down slope) and the path is not
+    this line, but only the dots on this line with vertical coordinate
+    multiple of mᵥ (jumping).
+    =#
+        
     mₕ = 3
     mᵥ = 1
     m = mₕ // mᵥ
